9229652a18dd3417e32488dacb016e4fa3f0b8e8
Shailaja Kant Singh <shailaja.s@algowire.com>
2020-12-07 15:33:59 +0530

    adding seed file to generating list & todo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+for i in 1..250 do
+  begin
+      list = List.new(name: "List number #{i}")
+
+      if list.save
+        for j in 1..15 do
+          todo = list.todos.build(description: "Todo number #{j} in list #{i}")
+          todo.save
+        end
+      end
+  rescue => exception
+    puts "exception occure"
+  end
+end
